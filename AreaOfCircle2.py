581c9803eb09332a3eba7e6c001c75f253e88520
Nikhil53003 <nikhil300855@gmail.com>
2024-05-09 00:55:16 +0530

    Add files via upload

---
+++ AreaOfCircle2.py
@@ -0,0 +1,8 @@
+radius = 84
+pi = 3.14
+circle_area = pi * (radius ** 2)
+print("Area of the pond:", circle_area)
+
+water_per_square_meter = 1.4
+total_water = int(circle_area * water_per_square_meter)
+print("Total amount of water in the pond:", total_water)
---
+++ functiondef2.py
@@ -0,0 +1,5 @@
+def in_string(num, char):
+    return "{:d}{}".format(num, char)
+
+result = in_string(145, 'o')
+print("Formatted String:", result)
