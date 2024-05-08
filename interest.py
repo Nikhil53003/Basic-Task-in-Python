8aa599cc2447827ce1329253d89c6392364f48c3
Nikhil53003 <nikhil300855@gmail.com>
2024-05-09 00:12:32 +0530

    Add files via upload

---
+++ interest.py
@@ -0,0 +1,6 @@
+principal_amount = 1000  
+rate_of_interest = 5     
+time = 3                 # Time in years
+
+simple_interest = (principal_amount * rate_of_interest * time) / 100
+print("Simple Interest:", simple_interest)
